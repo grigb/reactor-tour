737ce63ea70c1a3a8ebe420b0d0dd6b93a4bee69
Livi Erickson <Livi Erickson>
2016-11-02 14:19:19 -0700

    Add project

---
+++ Assets/360Shader.shader
@@ -0,0 +1,38 @@
+﻿Shader "Custom/360Shader" {
+	Properties {
+		_Color ("Color", Color) = (1,1,1,1)
+		_MainTex ("Albedo (RGB)", 2D) = "white" {}
+	}
+	SubShader {
+		Tags { "RenderType"="Opaque" }
+		Cull Front
+		
+		CGPROGRAM
+		// Physically based Standard lighting model, and enable shadows on all light types
+		#pragma surface surf Standard fullforwardshadows
+		#pragma surface surf Lambert vertex:vert
+		// Use shader model 3.0 target, to get nicer looking lighting
+		#pragma target 3.0
+
+		sampler2D _MainTex;
+
+		struct Input {
+			float2 uv_MainTex;
+			float4 color : COLOR;
+		};
+
+		void vert(inout appdata_full v)
+		{
+			v.normal.xyz = v.normal * -1;
+		}
+
+		void surf (Input IN, inout SurfaceOutputStandard o) {
+			// Albedo comes from a texture tinted by color
+			fixed3 result = tex2D(_MainTex, IN.uv_MainTex);
+			o.Albedo = result.rgb;
+			o.Alpha = 1;
+		}
+		ENDCG
+	}
+	FallBack "Diffuse"
+}
---
+++ Assets/ChangeRooms.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeRooms : MonoBehaviour {
+
+    public Location destination;
+    public enum Location { lobby, kitchen, desk, corner, lab};
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    // On "gaze ender / click method, move to next frame
+    // TODO: Add Teleport Function
+    public void OnClick()
+    {
+        Debug.Log("Teleporting to " + destination.ToString());
+        switch (destination)
+        {
+            case Location.lobby:
+                Camera.main.transform.position = new Vector3(0, 0, 0);
+                break;
+            case Location.kitchen:
+                Camera.main.transform.position = new Vector3(10, 0, -4);
+                break;
+            case Location.desk:
+                Camera.main.transform.position = new Vector3(20, 0, -4);
+                break;
+            case Location.corner:
+                Camera.main.transform.position = new Vector3(20, 0, -15);
+                break;
+            case Location.lab:
+                Camera.main.transform.position = new Vector3(10, 0, -15);
+                break;
+        }
+    }
+}
